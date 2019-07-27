2af21871098f8df4cd0926eb140b586fee608fa8
Marisol Poma <marisol.poma15@hotmail.com>
2019-07-26 22:29:43 -0300

    exemplo 3-ts

---
+++ dist/exemplo3.js
@@ -0,0 +1,5 @@
+var personagemPreferido;
+personagemPreferido = "Naruto";
+console.log("Meu personagem preferido é " + personagemPreferido);
+personagemPreferido = 0410;
+console.log("Meu personagem preferido é " + personagemPreferido);
---
+++ exemplo3.ts
@@ -0,0 +1,5 @@
+let personagemPreferido 
+personagemPreferido = "Naruto"
+console.log("Meu personagem preferido é " + personagemPreferido)
+personagemPreferido = 0410
+console.log("Meu personagem preferido é " + personagemPreferido)
